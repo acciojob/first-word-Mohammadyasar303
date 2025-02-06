2fd33d66ab2ffdcba50cf9b8722657f155b685f0
Mohammadyasar Mansuri <126454357+Mohammadyasar303@users.noreply.github.com>
2025-02-06 11:50:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function firstWord(s) {
   // your code here
+	if (s.length === 0) {
+    return s;
+  }
+
+  // Step 2: Trim the string to remove any leading or trailing spaces
+  s = s.trim();
+
+  // Step 3: Find the index of the first space in the string
+  let spaceIndex = s.indexOf(' ');
+
+  // Step 4: If there is no space in the string, return the entire string
+  if (spaceIndex === -1) {
+    return s;
+  }
+
+  // Step 5: If there is a space, return the substring from the start of the string to the index of the first space
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
